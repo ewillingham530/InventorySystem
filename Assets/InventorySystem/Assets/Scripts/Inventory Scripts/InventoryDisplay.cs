8f38d6be54e327b3fa713223c9801a4fba291616
Emily Willingham <ewillingham530@gmail.com>
2023-12-07 04:44:44 -0600

    Final Tweaks

--- Assets/InventorySystem/Assets/Scripts/Inventory Scripts/InventoryDisplay.cs
+++ Assets/InventorySystem/Assets/Scripts/Inventory Scripts/InventoryDisplay.cs
@@ -12,18 +12,7 @@ public abstract class InventoryDisplay : MonoBehaviour
     public Dictionary<InventoryManager, InventoryManager> SlotDictionary => slotDictionary;
     public abstract void AssignSlot();
 
-    //protected virtual void UpdateSlot(InventoryManager updatedSlot)
-    //{
-    //    foreach (var slot in SlotDictionary)
-    //    {
-    //        if (slot.Value == updatedSlot)      // slot value - inventory slot
-    //        {
-    //            slot.Key.UpdatedUISlot(updatedSlot);       //slot key, the ui rep of the value
-    //        }
-    //    }
-    //}
-
-    //public void SlotClicked
+    
    
 
 }
